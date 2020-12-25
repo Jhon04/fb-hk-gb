41eff7f808f3869dae0492652f44e64b626d18a3
JhonIvanChugnasMosqueira <chugnasivan@gmail.com>
2020-12-25 15:43:55 -0500

    version 1.0.0 - subida del proyecto

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+app = express().use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+
+    console.log('POST: WebHook')
+
+    const body = req.body;
+
+    if(body.object === 'page'){
+
+        body.forEach(entry => {
+            // re reciben y procesan los mensajes
+            const webhookEvent = entry.menssaging[0];
+            console.log(webhookEvent);
+        })
+        res.status(200).send('EVENTO RECIBIDO')
+    }else{
+        res.sendStatus(404);
+    }
+
+});
+
+app.get('/webhook', (req, res) => {
+
+    console.log('GET: WebHook')
+
+    const VERIFY_TOKEN ='<stringUnicoParaTuAplicacion>';
+    
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if(mode && token){
+        if(mode === 'subscribe' && token === VERIFY_TOKEN){
+            console.log('webhook VERIFICADO')
+            res.status(200).send(challenge);
+        }else{
+            res.sendStatus(404);
+        }
+    }else{
+        res.sendStatus(404);
+    }
+
+
+});
+
+app.listen(3000, () => {
+    console.log('servidor iniciado...')
+});
